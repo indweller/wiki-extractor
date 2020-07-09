5ac65bb7a6abea58206339d0ae66ea1fbd46393d
Prashanth Ravichandar <31028299+indweller@users.noreply.github.com>
2020-07-09 09:56:47 +0530

    Update feeder.sh

--- feeder.sh
+++ feeder.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 # Script to feed the 'count' and 'skip' values to the dd command.
 # The dd command will extract a small portion of the wiki dump
+# Change the name of the python file according to needs
 
-input="/mnt/d/Experiment/index.txt"
+input="/datapartition/surface-names/final/index.txt"
 cur=0
 prev=0
 while read line
 do
         prev=$cur
         cur=$line
-	if [ $prev -ge 8914543669 ]
+	if [ $prev -ne 0 ]
         then
-	        dd if=enwiki-20200101-pages-articles-multistream.xml.bz2 of=try1.xml.bz2 bs=1 count=$((cur-prev)) skip=$prev
+	        dd if=/datapartition/enWiki2016/enwiki-20161001-pages-articles-multistream.xml.bz2 of=try1.xml.bz2 bs=1 count=$((cur-prev)) skip=$prev
 	        bzip2 -d try1.xml.bz2
 	        python3 text-extractor.py
 	        rm try1.xml
-		echo "scale=2; (($cur*100/17751212678))" | bc -l
+		echo "scale=2; (($cur*100/14178593313))" | bc -l
 	fi
 done < "$input"
